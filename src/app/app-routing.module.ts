143d05ccd6c9350a7220cd9ac75e67ba5be4076a
Aleksa <lukicaleksa04@gmail.com>
2020-10-06 17:29:52 +0200

    Footer html/css

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AdminComponent } from './admin/admin.component';
+import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
 
 
 const routes: Routes = [
   {path:'',component:HomeComponent},
-  {path:'admin',component:AdminComponent}
+  {path:'admin',component:AdminComponent},
+  {path:'contact',component:FooterComponent}
 ];
 
 @NgModule({
