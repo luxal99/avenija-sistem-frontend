dea259fcc3dca0510f4ca3c2aaf5bdf00d565cd0
Aleksa <lukicaleksa04@gmail.com>
2020-10-11 01:29:01 +0200

    Binding on home html/css

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material';
+import { error } from 'protractor';
+import { SwiperOptions } from 'swiper';
+import { Estate } from '../models/Estate';
+import { EstateService } from '../service/estate.service';
 import { LoginDialogComponent } from './login-dialog/login-dialog.component';
 import { RegistrationDialogComponent } from './registration-dialog/registration-dialog.component';
-
+import {Image} from "src/app/models/Image"
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -10,9 +14,40 @@ import { RegistrationDialogComponent } from './registration-dialog/registration-
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private dialog:MatDialog) { }
+  listOfEstates: Array<Estate> = []
+  listOfImages: Array<Image> = [];
+
+  constructor(private dialog: MatDialog, private estateService: EstateService) { }
+
+
+  config: SwiperOptions = {
+    slidesPerView: 2,
+    spaceBetween: 50,
+    slidesPerGroup: 2,
+    loop: true,
+    loopFillGroupWithBlank: false,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    }
+  }
+
 
   ngOnInit() {
+    this.getAllEstates();
+  }
+
+  getAllEstates() {
+    this.estateService.getAll().subscribe(resp => {
+      this.listOfEstates = resp as Array<Estate>
+
+      console.log(resp);
+      
+      
+      for(const img of this.listOfEstates){
+        this.listOfImages.push(img.listOfImages[0])
+      }
+    })
   }
 
   openRegistrationDialog() {
